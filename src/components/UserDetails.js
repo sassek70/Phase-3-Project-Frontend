a3c83afd964ed35c8763ad135638852ee189d51a
Kevin Sasse <sassek70@gmail.com>
2022-11-17 18:20:48 -0500

    User details & add ingredient/recipe button

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,15 +1,19 @@
 import { useState } from "react"
+import { NavLink } from "react-router-dom"
+
 
 const UserDetails = ({id, name, baseUrl}) => {
     const [userRecipes, setUserRecipes] = useState([])
     const [userIngredients, setUserIngredients] = useState([])
     const [showUserPantry, setShowUserPantry] = useState(false)
     const [showUserCookbook, setShowUserCookbook] = useState(false)
+    const [showDetails, setShowDetails] = useState(false)
 
 
-    const getUserRecipes = () => {
-        console.log("recipes")
+    const getUserRecipes = (e) => {
+        e.stopPropagation()
         setShowUserCookbook((showUserCookbook) => !showUserCookbook)
+        setShowUserPantry(false)
 
         fetch(`${baseUrl}user/${id}/recipes`)
         .then(res => res.json())
@@ -17,13 +21,21 @@ const UserDetails = ({id, name, baseUrl}) => {
     }
 
     
-    const getUserIngredients = () => {
-        console.log(id)
+    const getUserIngredients = (e) => {
+        e.stopPropagation()
         setShowUserPantry((showUserPantry) => !showUserPantry)
+        setShowUserCookbook(false)
 
         fetch(`${baseUrl}user/${id}/ingredients`)
         .then(res => res.json())
         .then((userIngredientsList) => setUserIngredients(userIngredientsList))
+    }
+
+    const expandDetails = () => {
+        setShowDetails((showDetails) => !showDetails)
+        setShowUserPantry(false)
+        setShowUserCookbook(false)
+
     }
 
     const displayIngredients = userIngredients.map((ingredient) => <div key={ingredient.id}>{ingredient.name}</div>)
@@ -33,27 +45,37 @@ const UserDetails = ({id, name, baseUrl}) => {
 
     return(
         <>
-        <div>
+        <div onClick={expandDetails}>
             <h2>{name}</h2>
+            {showDetails ?
+            <>
             <button onClick={getUserRecipes}>Recipe List</button>
             <button onClick={getUserIngredients}>Ingredient List</button>
+            </>
+            :
+            <></>
+            }   
         </div>
         <div>
             {showUserPantry ? 
-            <>
+            <div>
+                <h4>{name}'s ingredients:</h4>
                 {displayIngredients}
-            </>
+                <NavLink to={`/addIngredient`} name="New Ingredient">Add Ingredient</NavLink>
+            </div>
             :
-             <span>You have no ingredients</span>
+            <></>
             }    
         </div>
         <div>
             {showUserCookbook ? 
             <>
+                <h4>{name}'s recipes:</h4>
                 {displayRecipes}
+                <NavLink to={`/addRecipe`} name="New Recipe">Add Recipe</NavLink>
             </>
             :
-             <span>You have no recipes</span>
+             <></>
             }    
         </div>
         </>
@@ -61,3 +83,5 @@ const UserDetails = ({id, name, baseUrl}) => {
 }
 
 export default UserDetails
+
+
