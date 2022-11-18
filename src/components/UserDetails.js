276de77b79c254dc7872daf24e7d400dc564e13f
Kevin Sasse <sassek70@gmail.com>
2022-11-18 15:28:02 -0500

    removed whitespace

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -164,20 +164,6 @@ const UserDetails = ({id, name, baseUrl}) => {
 
 
 
-
-{/* <Card style={{ width: '18rem' }}>
-<Card.Body>
-  <Card.Title>{recipe.name}</Card.Title>
-  <Card.Subtitle className="mb-2 text-muted">{cuisine.name}</Card.Subtitle>
-  <Card.Text>
-    Some quick example text to build on the card title and make up the
-    bulk of the card's content.
-  </Card.Text>
-  <Card.Link href="`/Recipes/${recipe.id}`">Card Link</Card.Link>
-  <Card.Link href="#">Another Link</Card.Link>
-</Card.Body>
-</Card> */}
-
 export default UserDetails
 
 
